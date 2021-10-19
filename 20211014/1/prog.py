f37994dbcc6ab210c4a9a45915ddbdf9e36ae1d5
s90190054 <root@DESKTOP-C8Q9IVE.localdomain>
2021-10-19 19:30:15 +0300

    modified #1

--- 20211014/1/prog.py
+++ 20211014/1/prog.py
@@ -1,11 +1,15 @@
-def fun(s, w, na, a_coeffs, nb, b_coeffs):
-    pass
+from fractions import Fraction
 
-    l = str(input())
-    lst = l.split(', ')
-    s, w, *other = lst
-    na, other = other[0], other[1:]
-    a_coeffs, other = other[:int(na)+1], other[int(na)+1:]
-    nb, b_coeffs = other[0], other[1:]
-    #print(s, w, na, a_coeffs, nb, b_coeffs)
+def fun(s, w, na, a_coeffs, nb, b_coeffs):
+    A = sum([Fraction(c) * Fraction(s) ** x for (c,x) in zip(reversed(a_coeffs), list(range(int(na) + 1)))])
+    B = sum([Fraction(c) * Fraction(s) ** x for (c,x) in zip(reversed(b_coeffs), list(range(int(nb) + 1)))])
+    return Fraction(A) / Fraction(B) == Fraction(w)
+
+l = str(input())
+lst = list(map(lambda s: s.strip(), l.split(',')))
+s, w, *other = lst
+na, other = other[0], other[1:]
+a_coeffs, other = other[:int(na)+1], other[int(na)+1:]
+nb, b_coeffs = other[0], other[1:]
+print(fun(s, w, na, a_coeffs, nb, b_coeffs))
 
