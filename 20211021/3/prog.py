546dd0768609bd24164adf7f802efa3950ba24e4
s90190054 <root@DESKTOP-C8Q9IVE.localdomain>
2021-10-27 16:42:57 +0300

    #3

--- 20211021/3/prog.py
+++ 20211021/3/prog.py
@@ -1,11 +1,17 @@
 from collections import Counter
 
 w = int(input())
-text = input()
-lst = text.strip().split()
-for i in lst:
-    dct.setdefault(i,0)
-    dct[i] += 1
-lst1 = Counter(lst)
-print(dct)
-print(dict(lst1))
+text = ''
+while (line := input()) != '':
+    for char in line:
+        if not (char.isalpha() or char in (' ','\n','\t')):
+            line = line.replace(char, ' ')
+    text += line
+lst = text.lower().split()
+cntr = dict(Counter(lst))
+dct = {k: v for k,v in cntr.items() if len(k) == w}
+if dct == {}:
+    print()
+else:
+    m = dct[max(dct, key=dct.get)]
+    print(*sorted([k for k,v in dct.items() if v == m]))
