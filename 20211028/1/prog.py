1cd5fb93797f1b0d090fab0ea8d02c26842d35ba
s90190054 <root@DESKTOP-C8Q9IVE.localdomain>
2021-10-28 19:18:15 +0300

    prog #1

---
+++ 20211028/1/prog.py
@@ -0,0 +1,12 @@
+def fib(m, n):
+    f0 = 1
+    f1 = 1
+    f2 = 2
+    i = 0
+    while True:
+        if i >= m:
+            yield f0
+        if i == n:
+            return f0
+        f0, f1, f2 = f1, f2, f1 + f2
+        i += 1
