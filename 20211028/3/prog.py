443b17b99cb89e2e4dc9cc6cdbf98b5b167da7f8
s90190054 <root@DESKTOP-C8Q9IVE.localdomain>
2021-11-08 15:16:49 +0300

    prog #3 upd

--- 20211028/3/prog.py
+++ 20211028/3/prog.py
@@ -1,2 +1,1 @@
-from itertools import product
-print(', '.join(sorted(filter(lambda s: s.count('TOR') == 2, [''.join(t) for t in product('TOR', repeat=int(input()))]))))
+print(', '.join(sorted(filter(lambda s: s.count('TOR') == 2, [''.join(t) for t in __import__('itertools').product('TOR', repeat=int(input()))]))))
