44a8153a71c5be10dcec54ab4e4dbdb688a73c7c
Порывай Максим practicum <s90190054@stud.cs.msu.ru>
2021-11-11 16:05:09 +0300

    first 11.11.2021

---
+++ 20211111/1/prog.py
@@ -0,0 +1,6 @@
+class OrderedComplex(complex):
+    def __lt__(self, other):
+        return True if self.real < other.real and self.imag < other.imag else False
+class OrderedComplexMul(OrderedComplex):
+    def __matmul__(self, other):
+        return self.real * other.real + self.imag * other.imag
---
+++ 20211111/2/prog.py
@@ -0,0 +1,34 @@
+class InvalidInput(Exception):
+    pass
+
+class BadTriangle(Exception):
+    pass
+
+def triangleSquare(s):
+    try:
+        (x1, y1), (x2, y2), (x3, y3) = eval(s)
+    except Exception:
+        raise InvalidInput
+    try:
+        a = ((x1 - x2) ** 2 + (y1 - y2) ** 2) ** 0.5
+        b = ((x1 - x3) ** 2 + (y1 - y3) ** 2) ** 0.5
+        c = ((x3 - x2) ** 2 + (y3 - y2) ** 2) ** 0.5
+        p = (a + b + c) / 2
+        sq = (p * (p - a) * (p - b) * (p - c)) ** 0.5
+    except Exception:
+        raise BadTriangle
+    if sq:
+        return sq
+    else:
+        raise BadTriangle
+    
+while True:
+    string = input()
+    try:
+        square = triangleSquare(string)
+    except InvalidInput:
+        print('Invalid input')
+    except BadTriangle:
+        print('Not a triangle')
+    else:
+        print(f'{square:.2f}')
