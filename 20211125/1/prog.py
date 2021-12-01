dd8576f780e80e26ce2b5acccbc47551fe3ea4b7
s90190054 <root@DESKTOP-C8Q9IVE.localdomain>
2021-12-01 23:12:57 +0300

    popravochka

--- 20211125/1/prog.py
+++ 20211125/1/prog.py
@@ -5,7 +5,8 @@ remains = seq[1:]
 L = len(remains)
 lst = []
 for i in range(N):
-    lst.append(remains[round(i*L/N):round((i+1)*L/N)])
+    #lst.append(remains[round(i*L/N):round((i+1)*L/N)])
+    lst.append(remains[i*L//N:(i+1)*L//N])
 lst.sort()
 st = seq[0:1] + b''.join(lst)
 sys.stdout.buffer.write(st)
