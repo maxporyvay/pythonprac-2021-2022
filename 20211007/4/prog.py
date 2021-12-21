14bcb00ab071ee2a1057776d2ad89bf4fa6c0693
maxporyvay <sevmaxclever@gmail.com>
2021-12-21 19:12:42 +0300

    URLS updated

--- 20211007/4/prog.py
+++ 20211007/4/prog.py
@@ -1,11 +1,13 @@
 from math import *
 
 def Calc(s, t, u):
-    def func(a):
+    def func(x):
         a = eval(s, globals(), {'x': x})
         b = eval(t, globals(), {'x': x})
         return eval(u, globals(), {'x': a, 'y': b})
     return func
 
 cort = eval(input())
-print((Calc(*cort))(eval(input())))
+z = eval(input())
+f = Calc(*cort)
+print(f(z))
