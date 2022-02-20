2de9e2e75fab3d25b5d94f447df8fcaba252c1a0
maxporyvay <sevmaxclever@gmail.com>
2022-02-20 20:52:11 +0300

    Hometask from 14.02.2022: Step 2
    При указании в качестве параметра командной строки имени ветки выводится содержимое объекта-последнего коммита данной ветки.

--- 20220214/1/parser.py
+++ 20220214/1/parser.py
@@ -1,6 +1,20 @@
+import sys
 import zlib
 from glob import iglob
 from os.path import basename, dirname
 
-for dr in iglob('.git/refs/heads/*'):
-    print(basename(dr))
+if len(sys.argv) == 1:
+    for dr in iglob('.git/refs/heads/*'):
+        print(basename(dr))
+elif len(sys.argv) > 1:
+    branch = sys.argv[1]
+    branchpath = '.git/refs/heads/' + branch
+    commit_name = ''
+    with open(branchpath) as bp:
+        commit_name = bp.readline().strip()
+    commit_name_dir, commit_name_file = commit_name[:2], commit_name[2:]
+    commit_filepath = '.git/objects/' + commit_name_dir + '/' + commit_name_file
+    obj = ''
+    with open(commit_filepath, 'rb') as cf:
+        obj = zlib.decompress(cf.read())
+    print(obj.decode())
