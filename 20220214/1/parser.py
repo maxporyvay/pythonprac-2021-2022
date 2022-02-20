ac9bb318cef66c76340641d5b45debfd00dadbdf
maxporyvay <sevmaxclever@gmail.com>
2022-02-20 22:42:45 +0300

    Hometask from 14.02.2022: Step 3
    Добавлен вывод дерева, на который указывает последний коммит выбранной ветки.

--- 20220214/1/parser.py
+++ 20220214/1/parser.py
@@ -3,6 +3,8 @@ import zlib
 from glob import iglob
 from os.path import basename, dirname
 
+SHIFT = "  "
+
 if len(sys.argv) == 1:
     for dr in iglob('.git/refs/heads/*'):
         print(basename(dr))
@@ -17,4 +19,23 @@ elif len(sys.argv) > 1:
     obj = ''
     with open(commit_filepath, 'rb') as cf:
         obj = zlib.decompress(cf.read())
-    print(obj.decode())
+    objtext = obj.decode()
+    print(objtext)
+    _, objtext_ = objtext.split('tree ')
+    if 'parent' in objtext:
+        treename, _ = objtext_.split('\nparent')
+    else:
+        treename, _ = objtext_.split('\nauthor')
+    print('tree', treename)
+    treename_dir, treename_file = treename[:2], treename[2:]
+    treename_filepath = '.git/objects/' + treename_dir + '/' + treename_file
+    obj = ''
+    with open(treename_filepath, 'rb') as tf:
+        obj = zlib.decompress(tf.read())
+    _, _, tail = obj.partition(b'\x00')
+    while tail:
+        treeobj, _, tail = tail.partition(b'\x00')
+        tmode, tname = treeobj.split()
+        num, tail = tail[:20], tail[20:]
+        print(f"{SHIFT}{tname.decode()} {tmode.decode()} {num.hex()}")
+
