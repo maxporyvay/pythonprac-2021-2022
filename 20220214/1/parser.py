5e4eefa52a6e87e18a1b4bb7e6d64e1d2a84dcb6
maxporyvay <sevmaxclever@gmail.com>
2022-02-20 20:31:56 +0300

    Hometask from 14.02.2022: Step 1
    Создан гит-парсер каталога, позволяющий просматривать названия всех веток

--- 20220214/1/parser.py
+++ 20220214/1/parser.py
@@ -1,22 +1,6 @@
 import zlib
-import sys
 from glob import iglob
-from os.path import join
+from os.path import basename, dirname
 
-repo = sys.argv[1]
-repopath = join(repo, "objects", "??", "*")
-for objname in iglob(repopath):
-    print(objname)
-    	
-    with open(objname, "rb") as objfile:
-        fullobj = zlib.decompress(objfile.read())
-        header, _, body = fullobj.partition(b'\x00')
-        objkind, size = header.split()
-        print(objkind.decode(), int(size))
-        if objkind == b'commit':
-    	    print(body.decode())
-        elif objkind == b'tree':
-            while(body):
-               treehdr, _, tail = body.partition(b'\x00')
-               gitid, body = tail[:20], tail[20:]
-               print(f'\t{treehdr.decode()} {gitid.hex()}')
+for dr in iglob('.git/refs/heads/*'):
+    print(basename(dr))
