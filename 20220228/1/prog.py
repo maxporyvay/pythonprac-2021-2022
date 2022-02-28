95e9ea5525e5a2e5923b3b7a888579917d98f390
maxporyvay <sevmaxclever@gmail.com>
2022-02-28 17:15:59 +0300

    3rd param added

--- 20220228/1/prog.py
+++ 20220228/1/prog.py
@@ -1,9 +1,9 @@
 import textdistance
 
 
-def dist(s1, s2):
+def dist(s1, s2, typo):
     return textdistance.levenshtein(s1, s2)
-
+    
 
 str1 = input()
 str2 = input()
@@ -11,4 +11,4 @@ str3 = input()
 assert ' ' not in str1
 assert ' ' not in str2
 
-res = dist(str1, str2)
+res = dist(str1, str2, str3)
