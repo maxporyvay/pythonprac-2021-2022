af0ef3e742c73ffb48ac3709cae1251754c62cae
maxporyvay <sevmaxclever@gmail.com>
2022-03-10 22:10:59 +0300

    Реализованы функции show, add

--- 20220305/1/prog.py
+++ 20220305/1/prog.py
@@ -29,11 +29,36 @@ class Repl(cmd.Cmd):
         args = shlex.split(arg, comments=True)
         if len(args) < 1:
             return
+        else:
+            if len(args) == 1 and args[0] == 'monsters':
+                for name, dct in self.monsters.items():
+                    for coords, hp in dct.items():
+                        print(f'{name} at ({coords[0]} {coords[1]}) hp {hp}')
+            else:
+                print('Failed')
 
     def do_add(self, arg):
         args = shlex.split(arg, comments=True)
         if len(args) < 1:
             return
+        else:
+            if len(args) == 8:
+                m, n, name, h, hp, c, cx, cy = args
+                if m == 'monster' and n == 'name' and h == 'hp' and c == 'coords' and hp.isdigit() and cx.isdigit() and cy.isdigit():
+                    hp = int(hp)
+                    cx = int(cx)
+                    cy = int(cy)
+                    if hp > 0 and cx in range(10) and cy in range(10):
+                        if name in self.monsters:
+                            self.monsters[name][(cx, cy)] = hp
+                        else:
+                            self.monsters[name] = {(cx, cy): hp}
+                    else:
+                        print('Failed')
+                else:
+                    print('Failed')
+            else:
+                print('Failed')
 
     def complete_attack(self, text, line, begidx, endidx):
         pass
