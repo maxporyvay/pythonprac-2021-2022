ad6f37adad37d3cfe429b6f6e4e8f82d86adb03f
maxporyvay <sevmaxclever@gmail.com>
2022-04-11 13:30:29 +0300

    Task #1 from 11.04.2022 made

---
+++ 20220411/1/modu/__init__.py
@@ -0,0 +1,9 @@
+import gettext
+
+translation = gettext.translation('tra', 'po', fallback=True)
+_, ngettext = translation.gettext, translation.ngettext
+
+def dialog():
+    while s := input(_("Input a string: ")):
+        n = len(s.split())
+        print(ngettext('Entered {} word', 'Entered {} words', n).format(n))
---
+++ 20220411/1/modu/__main__.py
@@ -0,0 +1,4 @@
+from modu.__init__ import dialog
+
+if __name__ == '__main__':
+    dialog()
