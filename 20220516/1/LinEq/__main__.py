3c5fe6bf542999ec7bca8c226199da1661c34658
maxporyvay <sevmaxclever@gmail.com>
2022-05-26 00:25:21 +0300

    final task finished

---
+++ 20220516/1/LinEq/__main__.py
@@ -0,0 +1,4 @@
+from LinEq import main
+
+if __name__ == '__main__':
+    main()
---
+++ 20220516/1/test_lineq.py
@@ -0,0 +1,16 @@
+import unittest
+from LinEq import solve
+
+class TestSqEq(unittest.TestCase):
+
+    def test_1(self):
+        self.assertEqual(solve(0, 5), None)
+
+    def test_2(self):
+        self.assertEqual(solve(0, 0), None)
+        
+    def test_3(self):
+        self.assertEqual(solve(1, 0), 0)
+        
+    def test_4(self):
+        self.assertEqual(solve(1, 1), -1)
